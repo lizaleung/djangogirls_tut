81661d6115849847cc230a2674107c86ca10f5b7
Liza Leung <liza.c.leung@gmail.com>
2017-09-03 11:23:41 +0000

    changes

--- tut1/urls.py
+++ tut1/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # url(r'^$', 'tut1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+
 ]
